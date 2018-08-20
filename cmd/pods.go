bd67b6e43305073c509eac18ce085b4c481719b3
sven <sven@so36.net>
2018-08-20 09:18:15 +0200

    code: WIP Flags for pods

---
+++ cmd/pods.go
@@ -0,0 +1,60 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var list bool // used for flags
+
+// podsCmd represents the pods command
+var podsCmd = &cobra.Command{
+	Use:   "pods",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		//fmt.Println("pods called")
+		flagset.Visit(checkFlags);
+	},
+}
+
+func checkFlags(f *Flag){
+  if(f.Name == "list"){
+    fmt.Println("pods list")
+  }
+}
+
+
+func init() {
+	rootCmd.AddCommand(podsCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// podsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	podsCmd.Flags().BoolP("&list", "l", false, "List all pods of all namespaces")
+}
