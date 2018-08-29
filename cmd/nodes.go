7276e66d0963931cb77712b3d5acc006f8aaa70f
sven <sven@so36.net>
2018-08-29 12:34:17 +0200

    meta: Improve help for ktl nodes

--- cmd/nodes.go
+++ cmd/nodes.go
@@ -27,12 +27,7 @@ import (
 var nodesCmd = &cobra.Command{
 	Use:   "nodes",
 	Short: "Interact with nodes",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Gives a overview about all nodes.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		//fmt.Println("nodes called")
                 getNODES()
